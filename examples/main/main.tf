8fbc3edd5ce3db8637b530e373cecead73bf8664
LichongQ <31929148+LichongQ@users.noreply.github.com>
2026-04-16 23:41:04 +0800

    Update main.tf

--- examples/main/main.tf
+++ examples/main/main.tf
@@ -8,7 +8,6 @@ terraform {
       version = "0.1.0"
     }
   }
-  required_version = ">= 1.5.0"
 }
 
 # Configure the eDME provider.
@@ -19,14 +18,17 @@ provider "dme" {
   password  = "your_password"            # eDME northbound user password
 }
 
+data "dme_vm" "template" {
+  vm_id = "urn:sites:0A1010ED:vms:i-00000003"
+}
+
 # Create a VM from a template with desired running status.
 resource "dme_vm" "example" {
-  template_id  = "urn:sites:461D0910:vms:i-000001BB"  # Template to clone from
+  template_id  = data.dme_vm.template.vm_id  # Template to clone from
   name         = "terraform-test-vm"
   description  = "VM created by Terraform from eDME template"
   location     = "urn:sites:461D0910:clusters:173"      # Target cluster
-  is_binding_host = false
-  status       = "running"  # Create and auto-boot; change to "stopped" to keep off
+  status       = "stopped"  # Create and auto-boot; change to "stopped" to keep off
 
   # Compute resources
   cpu_cores = 4
@@ -35,34 +37,6 @@ resource "dme_vm" "example" {
   # OS configuration
   os_type       = "Linux"
   os_version    = "1719"
-  guest_os_name = "CentOS 7.9 64bit"
-/*
-  # Guest OS customization
-  hostname = "test-vm-01"
-  password = "YourSecurePassword123!"
-
-  # Network configuration
-  port_group_id = "urn:sites:45E707F1:dvswitchs:1:portgroups:1"
-  ip_address    = "192.168.1.100"
-  netmask       = "255.255.255.0"
-  gateway       = "192.168.1.1"
-
-  # Disk configuration
-  disks {
-    sequence_num = 1
-    datastore_id = "urn:sites:461D0910:datastores:195"
-    capacity_gb  = 100
-    pci_type     = "VIRTIO"
-    indip_disk   = false
-    persistent_disk = true
-    is_thin      = true
-  }*/
-}
-
-/*
-# Query an existing VM using the data source.
-data "dme_vm" "existing" {
-  vm_id = "urn:sites:461D0910:vms:i-000001BB"
 }
 
 # Output the created VM details.
@@ -76,24 +50,5 @@ output "vm_status" {
   value       = dme_vm.example.status
 }
 
-output "vm_actual_id" {
-  description = "Actual eDME VM ID"
-  value       = dme_vm.example.vm_id
-}
 
-output "vm_ip" {
-  description = "Actual IP address of the VM"
-  value       = dme_vm.example.ip_address_actual
-}
 
-# Output the queried VM details from data source.
-output "existing_vm_name" {
-  description = "Name of the existing VM"
-  value       = data.dme_vm.existing.name
-}
-
-output "existing_vm_status" {
-  description = "Status of the existing VM"
-  value       = data.dme_vm.existing.status
-}
-*/
